9b783cb373922cb9484e7bd30c0a6c5b12a19f2f
raj-9693 <rajkumarnishad96930@gmail.com>
2025-01-18 12:37:08 +0530

    update my_class

---
+++ app/src/main/java/com/example/proteamfantasy11rj/pradctionapp/my_class.kt
@@ -0,0 +1,36 @@
+package com.example.proteamfantasy11rj.pradctionapp
+
+class my_class {
+
+    companion object {
+
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+         println("Hello Kotlin ")
+            var raj="Raj kumar"
+
+            raj="prean"
+            println(raj)
+
+
+
+        }
+
+    }
+
+
+
+
+//    companion object{
+//        @JvmStatic
+//        fun main(args:Array<String>){
+//            val  Name="raj"
+//
+//            println("hello raj, $Name")
+//            println("hello kuch be")
+//
+//        }
+//
+//    }
+}
